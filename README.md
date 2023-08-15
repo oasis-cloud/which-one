6d652cfc4f0165351de8821cac81c05626481303
oasis-cloud <oasis_lab@sina.com>
2023-08-15 16:23:09 +0800

    feat: update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # which one
 List the scripts in package.json and choose one to execute
+
+## Install
+
+```shell
+npm install -D @oasis-cloud/which-one
+```
+
+## Example
+
---
+++ bin/which-one.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const prompts = require('prompts')
+const path = require('path')
+const { exec, spawn } = require('child_process');
+
+function readPackageJson() {
+  const dir = require(path.join(process.cwd(), 'package.json'))
+  return dir.scripts
+}
+
+(async () => {
+  const scripts = readPackageJson()
+  console.log(scripts)
+  const scriptsShortCut = Object.keys(scripts).map(i => ({
+    title: i,
+    value: i
+  }))
+  const response = await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'which one',
+    choices: scriptsShortCut
+  })
+  if(response.value) {
+    exec('npm run ' + response.value, (err, stdout, stderr) => {
+      console.log(stdout)
+      console.log(stderr)
+    })
+  }
+})();
